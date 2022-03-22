0a1adea277f1ea8e5c17a487be8488970056edf5
abdulnazifSource <abdulnazifmail@gmail.com>
2022-03-22 20:02:17 +0100

    added support for json format

--- NazifAPIs/urls.py
+++ NazifAPIs/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('api/<int:version>/', include('apis.urls')),
+    path('api/<str:version>/', include('apis.urls')),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('rest-auth/', include('rest_auth.urls')),
