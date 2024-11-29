32219cc670262a88c7f5b911f1626a992fe87b8c
nazif abdulkadir <nazifmail2@gmail.com>
2024-11-29 22:48:02 +0100

    Removed wagtail urls

--- NazifAPIs/urls.py
+++ NazifAPIs/urls.py
@@ -20,9 +20,6 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 from django.urls import include, path, re_path
 from django.views.generic import RedirectView, TemplateView
-from wagtail import urls as wagtail_urls 
-from wagtail.admin import urls as wagtailadmin_urls
-from wagtail.documents import urls as wagtaildocs_urls
 
 """ """
 urlpatterns = [
@@ -33,15 +30,13 @@ urlpatterns = [
     path('profile.html', TemplateView.as_view(template_name='profile.html')),
     path('order.html', TemplateView.as_view(template_name='order.html')),
     path('login.html', TemplateView.as_view(template_name='login.html')),
+    path('register.html', TemplateView.as_view(template_name='register.html')),
     path('api/', include('apis.urls')),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
 #    path('rest-auth/', include('rest_auth.urls')),
 #    path('rest-auth/registration/', include('rest_auth.registration.urls')),
     path('salesapi/', include('shop.urls')),
-    path('cms/', include(wagtailadmin_urls)),
-    path('documents/', include(wagtaildocs_urls)),
-    path('pages/', include(wagtail_urls)),
     path('frontend', include('frontend.urls', namespace='frontend')),
 ] 
 
